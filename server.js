91a20705bf8e64198823f57188605aa59c31f3d7
Justinnn <hello@justinnn.dev>
2023-05-16 18:54:58 +0200

    Byebye mongoose :wave: hello mysql2 :wave:

--- server.js
+++ server.js
@@ -1,74 +1,114 @@
-  require('dotenv').config();
-  const express = require("express");
-  const app = express();
-  app.set("view engine", "ejs");
-  app.use(express.static("public"));
-  app.use(express.urlencoded({ extended: true }));
+require('dotenv').config();
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-  const Document = require("./models/Document");
-  const mongoose = require("mongoose");
-  const detectLang = require('lang-detector');
-  const langs = require('./languages.json')
+const mysql = require("mysql2"); // Use mysql2 package instead of mysql
+const detectLang = require('lang-detector');
+const langs = require('./languages.json');
 
-  mongoose.connect(process.env.mongoURL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+const connection = mysql.createConnection({
+  host: process.env.mysqlHost,
+  user: process.env.mysqlUser,
+  password: process.env.mysqlPassword,
+  database: process.env.mysqlDatabase,
+  authPlugins: {
+    mysql_clear_password: () => () => Buffer.from(process.env.mysqlPassword + '\0')
+  }
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+function deleteExpiredDocuments() {
+  const query = "DELETE FROM pastes WHERE created_at < NOW() - INTERVAL 7 DAY";
+  connection.query(query, (err) => {
+    if (err) {
+      console.error("Error deleting expired pastes:", err);
+    }
   });
+}
 
-  app.get("/", (req, res) => {
-    res.render("new");
-  });
+setInterval(deleteExpiredDocuments, 24 * 60 * 60 * 1000);
 
-  app.post("/save", async (req, res) => {
-    const value = req.body.value;
-    const lang = await detectLang(value);
-    const id = `${Math.random().toString(36).substring(2, 9)}${lang !== 'Unknown' ? langs[lang] : ''}`;
-    try {
-      const document = await Document.create({
-        value: value,
-        id: id
-      });
-      res.redirect(`/${id}`);
-    } catch (e) {
-      res.render("new", { value });
-    };
-  });
+app.get("/", (req, res) => {
+  res.render("new");
+});
 
-  app.get("/:id/duplicate", async (req, res) => {
-    const id = req.params.id;
-    try {
-      const document = await Document.findOne({
-        id: id,
-      });
-      res.render("new", { value: document.value });
-    } catch (e) {
-      res.redirect(`/${id}`);
-    };
-  });
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  const lang = await detectLang(value);
+  const id = `${Math.random().toString(36).substring(2, 9)}${lang !== 'Unknown' ? langs[lang] : ''}`;
+  try {
+    const query = "INSERT INTO pastes (id, value) VALUES (?, ?)";
+    connection.query(query, [id, value], (err) => {
+      if (err) {
+        console.error("Error saving pastes:", err);
+        res.render("new", { value });
+      } else {
+        res.redirect(`/${id}`);
+      }
+    });
+  } catch (e) {
+    res.render("new", { value });
+  }
+});
 
-  app.get("/:id/raw", async (req, res) => {
-    const id = req.params.id;
-    try {
-      const document = await Document.findOne({
-        id: id,
-      });
-      res.render("raw", { code: document.value });
-    } catch (e) {
-      res.redirect(`/`);
-    };
-  });
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const query = "SELECT value FROM pastes WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+      if (err || results.length === 0) {
+        res.redirect(`/${id}`);
+      } else {
+        res.render("new", { value: results[0].value });
+      }
+    });
+  } catch (e) {
+    res.redirect(`/${id}`);
+  }
+});
 
-  app.get("/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-      const document = await Document.findOne({
-        id: id,
-      });
+app.get("/:id/raw", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const query = "SELECT value FROM pastes WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+      if (err || results.length === 0) {
+        res.redirect(`/`);
+      } else {
+        res.render("raw", { code: results[0].value });
+      }
+    });
+  } catch (e) {
+    res.redirect(`/`);
+  }
+});
 
-      res.render("code-display", { code: document.value, id });
-    } catch (e) {
-      res.redirect("/");
-    };
-  });
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const query = "SELECT value FROM pastes WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+      if (err || results.length === 0) {
+        res.redirect("/");
+      } else {
+        res.render("code-display", { code: results[0].value, id });
+      }
+    });
+  } catch (e) {
+    res.redirect("/");
+  }
+});
 
-  app.listen(process.env.port, () => console.log(`The hastebin server is now running on port ${process.env.port}`));
+app.listen(process.env.port, () =>
+  console.log(`The hastebin server is now running on port ${process.env.port}`)
+);
