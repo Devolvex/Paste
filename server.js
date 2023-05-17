bac672cc5ba34de11f2fa73a731849377c4cd0ed
Justinnn <63072677+Jqstln@users.noreply.github.com>
2023-05-17 12:47:30 +0200

    starting the convert towards prisma

--- server.js
+++ server.js
@@ -1,39 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 const app = express();
+const prisma = new PrismaClient();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const mysql = require('mysql2');
 const detectLang = require('lang-detector');
 const langs = require('./languages.json');
 
-const connection = mysql.createConnection({
-  host: process.env.mysqlHost,
-  user: process.env.mysqlUser,
-  password: process.env.mysqlPassword,
-  database: process.env.mysqlDatabase,
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from(process.env.mysqlPassword + '\0')
-  }
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
-
 function deleteExpiredDocuments() {
-  const query = 'DELETE FROM pastes WHERE created_at < NOW() - INTERVAL 7 DAY';
-  connection.query(query, (err) => {
-    if (err) {
+  prisma.paste.deleteMany({
+    where: {
+      createdAt: {
+        lt: new Date(new Date() - 7 * 24 * 60 * 60 * 1000),
+      },
+    },
+  })
+    .then(() => {
+      console.log('Expired documents deleted');
+    })
+    .catch((err) => {
       console.error('Error deleting expired pastes:', err);
-    }
-  });
+    });
 }
 
 setInterval(deleteExpiredDocuments, 24 * 60 * 60 * 1000);
@@ -47,68 +38,71 @@ app.post('/save', async (req, res) => {
   const lang = await detectLang(value);
   const id = `${Math.random().toString(36).substring(2, 9)}${lang !== 'Unknown' ? langs[lang] : ''}`;
   try {
-    const query = 'INSERT INTO pastes (id, value) VALUES (?, ?)';
-    connection.query(query, [id, value], (err) => {
-      if (err) {
-        console.error('Error saving pastes:', err);
-        res.render('new', { value });
-      } else {
-        res.redirect(`/${id}`);
-      }
+    await prisma.paste.create({
+      data: {
+        id,
+        value,
+      },
     });
+    res.redirect(`/${id}`);
   } catch (e) {
+    console.error('Error saving paste:', e);
     res.render('new', { value });
   }
 });
 
-app.get("/:id/duplicate", (req, res) => {
+app.get("/:id/duplicate", async (req, res) => {
   const id = req.params.id;
   try {
-    const query = 'SELECT value FROM pastes WHERE id = ?';
-    connection.query(query, [id], (err, results) => {
-      if (err || results.length === 0) {
-        res.redirect(`/${id}`);
-      } else {
-        res.render('new', { value: results[0].value });
-      }
+    const paste = await prisma.paste.findUnique({
+      where: { id },
     });
+    if (!paste) {
+      res.redirect(`/${id}`);
+    } else {
+      res.render('new', { value: paste.value });
+    }
   } catch (e) {
+    console.error('Error fetching paste:', e);
     res.redirect(`/${id}`);
   }
 });
 
-app.get('/:id/raw', (req, res) => {
+app.get('/:id/raw', async (req, res) => {
   const id = req.params.id;
   try {
-    const query = 'SELECT value FROM pastes WHERE id = ?';
-    connection.query(query, [id], (err, results) => {
-      if (err || results.length === 0) {
-        res.redirect(`/`);
-      } else {
-        res.render('raw', { code: results[0].value });
-      }
+    const paste = await prisma.paste.findUnique({
+      where: { id },
     });
+    if (!paste) {
+      res.redirect(`/`);
+    } else {
+      res.render('raw', { code: paste.value });
+    }
   } catch (e) {
+    console.error('Error fetching paste:', e);
     res.redirect(`/`);
   }
 });
 
-app.get('/:id', (req, res) => {
+app.get('/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const query = 'SELECT value FROM pastes WHERE id = ?';
-    connection.query(query, [id], (err, results) => {
-      if (err || results.length === 0) {
-        res.redirect('/');
-      } else {
-        res.render('code-display', { code: results[0].value, id });
-      }
+    const paste = await prisma.paste.findUnique({
+      where: { id },
     });
+    if (!paste) {
+      res.redirect('/');
+    } else {
+      res.render('code-display', { code: paste.value, id });
+    }
   } catch (e) {
+    console.error('Error fetching paste:', e);
     res.redirect('/');
   }
 });
 
-app.listen(process.env.port, () =>
-  console.log(`The hastebin server is now running on port ${process.env.port}`)
-);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`The hastebin server is now running on port ${port}`);
+});
