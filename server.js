304ee5a119f8dde2045200f5dc4757795ccf9651
Justinnn <hello@justinnn.dev>
2023-06-08 12:41:36 +0200

    feat: prisma is almost working

--- server.js
+++ server.js
@@ -2,27 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const app = express();
-const prisma = new PrismaClient();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const detectLang = require('lang-detector');
+const prisma = new PrismaClient();
+
 const langs = require('./languages.json');
+var detectLang = require('lang-detector');
 
 function deleteExpiredDocuments() {
-  prisma.paste.deleteMany({
-    where: {
-      createdAt: {
-        lt: new Date(new Date() - 7 * 24 * 60 * 60 * 1000),
-      },
-    },
-  })
-    .then(() => {
-      console.log('Expired documents deleted');
-    })
-    .catch((err) => {
+  prisma.paste.deleteMany({ where: { created_at: { lt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000) } } })
+    .catch(err => {
       console.error('Error deleting expired pastes:', err);
     });
 }
@@ -35,18 +27,13 @@ app.get('/', (req, res) => {
 
 app.post('/save', async (req, res) => {
   const value = req.body.value;
-  const lang = await detectLang(value);
+  const lang = detectLang(value);
   const id = `${Math.random().toString(36).substring(2, 9)}${lang !== 'Unknown' ? langs[lang] : ''}`;
   try {
-    await prisma.paste.create({
-      data: {
-        id,
-        value,
-      },
-    });
+    await prisma.paste.create({ data: { id, value } });
     res.redirect(`/${id}`);
-  } catch (e) {
-    console.error('Error saving paste:', e);
+  } catch (err) {
+    console.error('Error saving pastes:', err);
     res.render('new', { value });
   }
 });
@@ -54,16 +41,13 @@ app.post('/save', async (req, res) => {
 app.get("/:id/duplicate", async (req, res) => {
   const id = req.params.id;
   try {
-    const paste = await prisma.paste.findUnique({
-      where: { id },
-    });
-    if (!paste) {
-      res.redirect(`/${id}`);
-    } else {
+    const paste = await prisma.paste.findUnique({ where: { id } });
+    if (paste) {
       res.render('new', { value: paste.value });
+    } else {
+      res.redirect(`/${id}`);
     }
-  } catch (e) {
-    console.error('Error fetching paste:', e);
+  } catch (err) {
     res.redirect(`/${id}`);
   }
 });
@@ -71,38 +55,31 @@ app.get("/:id/duplicate", async (req, res) => {
 app.get('/:id/raw', async (req, res) => {
   const id = req.params.id;
   try {
-    const paste = await prisma.paste.findUnique({
-      where: { id },
-    });
-    if (!paste) {
-      res.redirect(`/`);
-    } else {
+    const paste = await prisma.paste.findUnique({ where: { id } });
+    if (paste) {
       res.render('raw', { code: paste.value });
+    } else {
+      res.redirect('/');
     }
-  } catch (e) {
-    console.error('Error fetching paste:', e);
-    res.redirect(`/`);
+  } catch (err) {
+    res.redirect('/');
   }
 });
 
 app.get('/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const paste = await prisma.paste.findUnique({
-      where: { id },
-    });
-    if (!paste) {
-      res.redirect('/');
-    } else {
+    const paste = await prisma.paste.findUnique({ where: { id } });
+    if (paste) {
       res.render('code-display', { code: paste.value, id });
+    } else {
+      res.redirect('/');
     }
-  } catch (e) {
-    console.error('Error fetching paste:', e);
+  } catch (err) {
     res.redirect('/');
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`The hastebin server is now running on port ${port}`);
-});
+app.listen(process.env.port, () =>
+  console.log(`The hastebin server is now running on port ${process.env.port}`)
+);
